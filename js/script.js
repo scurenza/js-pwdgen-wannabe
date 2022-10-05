8959ee2c05c6a9ed9ec1a83ed1679dfa3062c618
scurenza <fedechiar@hotmail.it>
2022-10-05 15:53:34 +0200

    esercizio completo

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-alert("ciao")
+//Chiedi all’utente il suo nome, poi chiedi il suo cognome, poi chiedi il suo colore preferito. Infine scrivi sulla pagina nomecognomecolorepreferito21
+
+// Dati
+const fixedNumber = 21;
+
+//Input
+const name = prompt("Qual è il tuo nome?");
+const surname = prompt("Qual è il tuo cognome?");
+const color = prompt("Qual è il tuo colore preferito?");
+
+// Elaborazione
+const psw = `La tua nuova password sarà ${name}${surname}${color}${fixedNumber}`;
+console.log(psw);
+
+//Output
+document.getElementById('psw').innerHTML = psw;
